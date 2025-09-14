30be2f360ad076f07b31fc251149bd6aa115aa4b
Anish-17032003 <anishsihota55@gmail.com>
2025-09-14 13:41:52 +0530

    Update food.routes.js

--- backend/src/routes/food.routes.js
+++ backend/src/routes/food.routes.js
@@ -12,11 +12,11 @@ const upload=multer({
 
 
 // aur yeh jo hamari api h isse hamara food item add hora h to food item add kon kr rha h food partner to isliye yeh hamari protected honi chahiyeee
-router.post('/',authMiddleware.authFoodPartnerMiddleware,upload.single("video"),foodController.createFood); //string ke andr jo video h wo aeve hi nhi diya blki jo frontend me uska naam h jese ki humne postman me uska video se naam save kiya h video ka to humx isme wahi daal skte h
+router.post('/home',authMiddleware.authFoodPartnerMiddleware,upload.single("video"),foodController.createFood); //string ke andr jo video h wo aeve hi nhi diya blki jo frontend me uska naam h jese ki humne postman me uska video se naam save kiya h video ka to humx isme wahi daal skte h
 
 /* GET /api/food/ [protected ] yeh hum bnaynge users ke liye ki jab user scroll krenge to jo jo nayi videos ayngi scroll krte krte wo sari videos ka data lane ke liye yeh api madat kregi aur yeh bhi protected banegi */
 
-router.get('/',authMiddleware.authUserMiddleware,foodController.getFoodItems); 
+router.get('/home',authMiddleware.authUserMiddleware,foodController.getFoodItems); 
 
 
 router.post('/like',authMiddleware.authUserMiddleware,foodController.likeFood);
