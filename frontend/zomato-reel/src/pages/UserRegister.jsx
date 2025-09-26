9f2436c325efb5c55288a0504b15d5903b25c85d
Anish-17032003 <anishsihota55@gmail.com>
2025-09-26 19:53:02 +0530

    Update UserRegister.jsx

--- frontend/zomato-reel/src/pages/UserRegister.jsx
+++ frontend/zomato-reel/src/pages/UserRegister.jsx
@@ -12,7 +12,7 @@ const UserRegister = () => {
         const fullname=e.target.fullname.value;
         const email=e.target.email.value;
         const password=e.target.password.value;
-        const response=await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/user/register`,{
+        const response=await axios.post('https://zomato-reels-backend5.onrender.com/api/auth/user/register',{
             fullname, email, password
     },{withCredentials:true})
     console.log(response.data);
